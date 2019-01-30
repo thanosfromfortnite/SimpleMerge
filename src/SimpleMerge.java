6848bc964f6241e39e1b2fd98f264634915ed4fa
BT_1N3_24 <fg>
2019-01-30 10:13:24 -0500

    d

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -6,7 +6,35 @@
  */
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        boolean done = false;
+        int[] out = new int[arr1.length + arr2.length];
+        while (!done) {
+            if (arr1[i] < arr2[j]) {
+                out[i + j] = arr1[i];
+                i ++;
+            }
+            else {
+                out[i + j] = arr2[j];
+                j ++;
+            }
 
+            if (j == arr2.length - 1) {
+                for (i = i; i < arr1.length; i ++) {
+                    out[i + j] = arr1[i];
+                }
+                done = true;
+            }
+
+            if (i == arr1.length - 1) {
+                for (j = j; j < arr2.length; j ++) {
+                    out[i + j] = arr2[j];
+                }
+                done = true;
+            }
+
+        }
+        return out;
     }
 
 }
